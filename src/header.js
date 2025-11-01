e9c5fcfe2b62ded41be4f2614d14d460b0667d53
Qhh-Brucep <brucep@qualityhomesforhumankind.org>
2025-11-01 15:54:41 +0000

    clean up page

--- src/header.js
+++ src/header.js
@@ -1,20 +1,19 @@
 import './style.css'
 import wgaf from './images/WGAF.png'
-import qhh from './images/QHH.png'
+import qhh from './images/QHHBLACK.png'
 
 const headerItem = document.querySelector('#Header')
 
-if (headerItem.dataset.pageType == "qhh") {
-  headerItem.innerHTML = `
-    <header>
-      <img src="${qhh}" class="h-12 logo" alt="Vite logo"/>
-    </header>
-  `
-} else if(headerItem.dataset.pageType == "wgaf"){
-  headerItem.innerHTML = `
-    <header>
-      <img src="${wgaf}" class="h-12 logo" alt="Vite logo"/>
-    </header>
-  `
+if (!headerItem) {
+  throw new Error('Header element not found')
 }
-setupCounter(document.querySelector('#counter'))
+
+const pageType = headerItem.dataset.pageType
+const logoSrc = pageType === 'wgaf' ? wgaf : qhh
+const logoAlt = pageType === 'wgaf' ? 'WGAF logo' : 'Quality Homes For Human Kind logo'
+
+headerItem.innerHTML = `
+  <div class="header-container flex items-center justify-center p-4">
+    <img src="${logoSrc}" class="h-12 logo" alt="${logoAlt}" />
+  </div>
+`
